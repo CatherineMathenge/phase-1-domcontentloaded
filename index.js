d77c9b095729931504273395d71d4f2158cc21c7
Catherine <catherine.mathenge@strathmore.edu>
2023-07-07 15:22:32 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+function changeTextOnLoad() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = "This is really cool!";
+  });
+}
+
+// Call the function to change the text on load
+changeTextOnLoad();
